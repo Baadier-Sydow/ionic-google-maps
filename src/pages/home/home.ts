bf6b6bd8f15bffa58d1251cd848171f420306785
Baadier Sydow <bsydow@gmail.com>
2018-01-18 20:14:13 +0200

    Get the users current location

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  location: {
+    latitude: number,
+    longitude: number
+  };
+  
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+    this.findUserLocation();
+  }
 
-  constructor(public navCtrl: NavController) {
+  findUserLocation(){
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 25000
+    };
 
+
+    this.geolocation.getCurrentPosition(options).then((position) => {
+
+      this.location = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      };
+
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
   }
 
 }
