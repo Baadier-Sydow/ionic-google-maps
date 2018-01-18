10403c69e5316dbf7306fbd0510aa65287329376
Baadier Sydow <bsydow@gmail.com>
2018-01-18 23:50:55 +0200

    Initialize the map into the view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { MapsProvider } from './../../providers/maps/maps';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
+
   location: {
     latitude: number,
     longitude: number
   };
+
+  @ViewChild('map') mapElement: ElementRef;
   
-  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public geolocation: Geolocation, public mapsProvider: MapsProvider) {
+    
+  }
+
+  ionViewDidLoad() {
     this.findUserLocation();
   }
 
@@ -30,6 +37,8 @@ export class HomePage {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       };
+
+      this.mapsProvider.init(this.location, this.mapElement);
 
      }).catch((error) => {
        console.log('Error getting location', error);
