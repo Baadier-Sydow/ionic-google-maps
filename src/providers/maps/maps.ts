66ef5d5ffbc0faa731a29a3b7668376b59101c7e
Baadier Sydow <bsydow@gmail.com>
2018-01-18 23:49:50 +0200

    Initialize correct maps provider based on device

--- src/providers/maps/maps.ts
+++ src/providers/maps/maps.ts
@@ -1,17 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { Platform } from 'ionic-angular';
 import { Injectable } from '@angular/core';
+import { JsMapsProvider } from './../js-maps/js-maps';
+import { NativeMapsProvider } from './../native-maps/native-maps';
+import { GoogleMaps } from '@ionic-native/google-maps';
 
-/*
-  Generated class for the MapsProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class MapsProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello MapsProvider Provider');
+  map: any;
+
+  constructor(public platform: Platform) {
+    if(this.platform.is('cordova') && 
+      (this.platform.is('ios') || this.platform.is('android'))){
+      this.map = new NativeMapsProvider(GoogleMaps);
+    } else {
+      this.map = new JsMapsProvider();
+    }
+  }
+
+  init(location, element){
+    this.map.init(location, element);
   }
 
 }
