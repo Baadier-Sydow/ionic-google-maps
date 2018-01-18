d729cec388ab22620f49624b95a6a2670fe6f778
Baadier Sydow <bsydow@gmail.com>
2018-01-18 23:50:08 +0200

    Setup native handling of Google Maps

--- src/providers/native-maps/native-maps.ts
+++ src/providers/native-maps/native-maps.ts
@@ -1,17 +1,32 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { GoogleMaps, LatLng, GoogleMapsEvent } from '@ionic-native/google-maps';
 
-/*
-  Generated class for the NativeMapsProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class NativeMapsProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello NativeMapsProvider Provider');
+  map: any;
+
+  constructor(public googleMaps: GoogleMaps) {
+
+  }
+
+  init(location, element){
+
+    let latLng = new LatLng(location.latitude, location.longitude);
+
+    let opts = {
+      camera: {
+        latLng: latLng,
+        zoom: 11,
+        tilt: 30
+      }
+    };
+
+    this.map = this.googleMaps.create(element.nativeElement, opts);
+
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+      console.log('Map is ready!');
+    });
   }
 
 }
